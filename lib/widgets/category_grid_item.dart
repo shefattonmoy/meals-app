c2a0981206aef70092adff2856310cf31cf328fc
shefattonmoy <shefathossain7@gmail.com>
2025-10-29 12:11:56 +0600

    app initialized with category list

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({required this.category, super.key});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withAlpha((0.55 * 255).round()),
+            category.color.withAlpha((0.99 * 255).round()),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onSurface,
+        ),
+      ),
+    );
+  }
+}
